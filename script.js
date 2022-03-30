887c2e8d71ae5b56d217d916d5c3f92c0cb27af9
icorvus <business.wrona@gmail.com>
2022-03-30 22:27:56 +0200

    Added prototype function to display Books in DOM

--- script.js
+++ script.js
@@ -2,18 +2,27 @@
 
 const modalContainer = document.getElementById('modal-container');
 const modal = document.getElementById('modal');
+const addBtn = document.getElementById('add-btn');
+const closeModalBtn = document.getElementById('close-modal-btn');
+const modalReadBtn = document.getElementById('modal-read');
 
 function toggleModal() {
   modalContainer.classList.toggle('hide');
 }
 
-const addBtn = document.getElementById('add-btn');
-addBtn.addEventListener('click', toggleModal);
+function toggleRead(event) {
+  event.target.classList.toggle('read');
+  console.log('work work')
+}
 
+
+addBtn.addEventListener('click', toggleModal);
 modalContainer.addEventListener('click', toggleModal);
+closeModalBtn.addEventListener('click', toggleModal);
 modal.addEventListener('click', (event) => {
   event.stopPropagation();
 })
+modalReadBtn.addEventListener('click', toggleRead);
 
 
 
@@ -21,7 +30,8 @@ let myLibrary = [];
 
 const testBook1 = new Book("The Hobbit", "Tolkien", 432, "true");
 const testBook2 = new Book("Hamlet", "William Shakespeare", 102, "false");
-myLibrary.push(testBook1, testBook2);
+const testBook3 = new Book("The Little Prince", "Antoine de Saint-Exupery", 154, "true");
+myLibrary.push(testBook1, testBook2, testBook3);
 
 function Book(title, author, numberOfPages, read) {
   this.title = title;
@@ -29,6 +39,46 @@ function Book(title, author, numberOfPages, read) {
   this.numberOfPages = numberOfPages;
   this.read = read;
 }
+
+Book.prototype.addToDOM = function() {
+  const cardContainer = document.querySelector('.card-container');
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const title = document.createElement('h2');
+  title.classList.add('title');
+  title.textContent = this.title;
+
+  const author = document.createElement('div');
+  author.classList.add('author');
+  author.textContent = `by ${this.author}`;
+
+  const pages = document.createElement('div');
+  pages.classList.add('pages');
+  pages.textContent = `${this.numberOfPages} pages`;
+
+  const controls = document.createElement('div');
+  controls.classList.add('controls');
+
+  const readBtn = document.createElement('button');
+  readBtn.classList.add('btn', 'read-btn');
+  readBtn.textContent = 'Read';
+  if (this.read) readBtn.classList.add('read');
+
+  const svgSpan = document.createElement('span');
+  svgSpan.innerHTML = "<svg style=\"width:24px;height:24px\" viewBox=\"0 0 24 24\"\><path fill=\"currentColor\" d=\"M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z\" /></svg>"
+
+  controls.appendChild(readBtn);
+  controls.appendChild(svgSpan);
+
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(controls);
+
+  cardContainer.appendChild(card);
+};
 
 function addBookToLibrary() {
   const title = prompt("Title: ");
