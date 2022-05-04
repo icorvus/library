1ecebd27d5a9a35b24682afec170c8443c8cd408
icorvus <business.wrona@gmail.com>
2022-05-04 16:35:09 +0200

    Fixed typos

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ const closeModalBtn = document.getElementById('close-modal-btn');
 const modalReadBtn = document.getElementById('modal-read');
 const saveBtn = document.getElementById('save-btn');
 
+addBtn.addEventListener('click', toggleModal);
+modalContainer.addEventListener('click', toggleModal);
+closeModalBtn.addEventListener('click', toggleModal);
+modal.addEventListener('click', (event) => {
+  event.stopPropagation();
+});
+modalReadBtn.addEventListener('click', toggleReadClass);
+saveBtn.addEventListener('click', addBookToLibrary);
+
 function toggleModal() {
   modalContainer.classList.toggle('hide');
 }
@@ -14,24 +23,6 @@ function toggleModal() {
 function toggleReadClass(event) {
   event.target.classList.toggle('read');
 }
-
-addBtn.addEventListener('click', toggleModal);
-modalContainer.addEventListener('click', toggleModal);
-closeModalBtn.addEventListener('click', toggleModal);
-modal.addEventListener('click', (event) => {
-  event.stopPropagation();
-})
-modalReadBtn.addEventListener('click', toggleReadClass);
-saveBtn.addEventListener('click', addBookToLibrary);
-
-
-
-let myLibrary = [];
-
-const testBook1 = new Book("The Hobbit", "Tolkien", 432, "true");
-const testBook2 = new Book("Hamlet", "William Shakespeare", 102, "false");
-const testBook3 = new Book("The Little Prince", "Antoine de Saint-Exupery", 154, "true");
-
 
 function Book(title, author, numberOfPages, read) {
   this.title = title;
@@ -69,10 +60,10 @@ Book.prototype.addToDOM = function(bookIndex) {
   readBtn.addEventListener('click', toggleReadClass);
   readBtn.addEventListener('click', () => {
     this.read = !this.read;
-  })
+  });
 
   const svgSpan = document.createElement('span');
-  svgSpan.innerHTML = "<svg style=\"width:24px;height:24px\" viewBox=\"0 0 24 24\"\><path fill=\"currentColor\" d=\"M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z\" /></svg>"
+  svgSpan.innerHTML = "<svg style=\"width:24px;height:24px\" viewBox=\"0 0 24 24\"\><path fill=\"currentColor\" d=\"M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z\" /></svg>";
   svgSpan.addEventListener('click', () => {
     myLibrary.splice(bookIndex, 1);
     reloadLibrary();
@@ -102,24 +93,6 @@ function addBookToLibrary() {
   reloadLibrary();
 }
 
-function addBookToLibrsary() {
-  const title = prompt("Title: ");
-  const author = prompt("Author: ");
-  const numberOfPages = prompt("Number of Pages: ");
-  const read = prompt("Have you read it yet: ");
-
-  myLibrary.push(new Book(title, author, numberOfPages, read));
-}
-
-function removeBook(event) {
-  console.log(event.target);
-  myLibrary.splice(event.target.getAttribute('data-index'), 1);
-  reloadLibrary();
-}
-
-myLibrary.push(testBook1, testBook2, testBook3);
-displayBooks();
-
 function displayBooks() {
   myLibrary.forEach((book, index) => book.addToDOM(index));
 }
@@ -128,3 +101,12 @@ function reloadLibrary () {
   document.querySelector('.card-container').textContent = "";
   displayBooks();
 }
+
+let myLibrary = [];
+
+const testBook1 = new Book("The Hobbit", "Tolkien", 432, "true");
+const testBook2 = new Book("Hamlet", "William Shakespeare", 102, "false");
+const testBook3 = new Book("The Little Prince", "Antoine de Saint-Exupery", 154, "true");
+
+myLibrary.push(testBook1, testBook2, testBook3);
+displayBooks();
